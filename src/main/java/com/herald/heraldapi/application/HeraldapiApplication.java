3e36175484bb41dbe1de22c74f9af84287848f49
Cem Pesket <pesketcem@gmail.com>
2018-07-03 22:20:38 +0300

    security config added
    security config added with memory auth

--- src/main/java/com/herald/heraldapi/application/HeraldapiApplication.java
+++ src/main/java/com/herald/heraldapi/application/HeraldapiApplication.java
@@ -1,4 +1,4 @@
-package com.herald.heraldapi.application;
+package com.herald.heraldapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
